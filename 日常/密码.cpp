1507c2c89476a6d4c8ad774e0ef312ce72ac304d
sinsong <yuhaoranm@live.com>
2018-11-27 16:23:07 +0800

    update

---
+++ 日常/密码.cpp
@@ -0,0 +1,50 @@
+// test.cpp : 定义控制台应用程序的入口点。
+//
+
+#include <stdio.h>
+#include <string.h>
+
+#define magic_number 12138
+#define buff_size 20
+
+int main(int argc, char* argv[])
+{
+	const char *passwd = "123456";
+
+	int count = 0;
+	int pass = 0;
+
+	while(count < 3)
+	{
+		char input[buff_size];
+
+		printf("input password:");
+		fgets(input, buff_size, stdin);
+
+		for(int i = 0; i < buff_size ; ++i)
+		{ if(input[i] == '\n') input[i] = '\0';}
+
+		if(strcmp(input, passwd) == 0)
+		{
+			pass = magic_number;
+			break;
+		}
+		else
+		{
+			printf("error. input again\n");
+			++count;
+		}
+	}
+
+	if(pass == magic_number)
+	{
+		printf("pass\n");
+	}
+	else
+	{
+		printf("over\n");
+	}
+
+	return 0;
+}
+
---
+++ 日常/玩数组.cpp
@@ -0,0 +1,152 @@
+// test.cpp : 定义控制台应用程序的入口点。
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NDATA 15
+
+int main(int argc, char* argv[])
+{
+	int data[NDATA] = { 0 };
+
+	for (int i = 0; i < NDATA; ++i)
+	{
+		scanf("%d", &data[i]);
+	}
+
+	printf("\n");
+	//---------------------------------------
+	printf("\nfunction 1:\n");
+
+	for (int i = 0; i < NDATA; ++i)
+	{
+		printf("%d ", data[i]);
+	}
+
+	printf("\n");
+	//---------------------------------------
+	printf("\nfunction 2:\n");
+
+	for (int i = NDATA - 1; i >= 0; --i)
+	{
+		printf("%d ", data[i]);
+	}
+
+	printf("\n");
+	//---------------------------------------
+	printf("\nfunction 3:\n");
+
+	int found = 0;
+	for (int i = 0; i < NDATA; ++i)
+	{
+		if (data[i] == 100)
+		{
+			printf("find 100 at -> %d", i);
+			found = 1;
+			break;
+		}
+	}
+	if (found != 1) printf("not find 100");
+
+	printf("\n");
+	//---------------------------------------
+	printf("\nfunction 4:\n");
+
+	int sum = 0;
+	for (int i = 0; i < NDATA; ++i)
+	{
+		if (data[i] >= 90)
+		{
+			++sum;
+		}
+	}
+	printf("优秀人数: %d", sum);
+
+	printf("\n");
+	//---------------------------------------
+	printf("\nfunction 5:\n");
+
+	for (int i = 0; i < NDATA; ++i)
+	{
+		if (data[i] < 60)
+		{
+			data[i] = -1;
+		}
+	}
+
+	while (1)
+	{
+		int n = 0, e = NDATA - 1;
+		while (e > 0 && data[e] == -1) e--;
+		while (n < NDATA && data[n] != -1) n++;
+		if (n == e + 1)
+		{
+			break;
+		}
+		//swap n,e
+		data[n] = data[e];
+		data[e] = -1;
+	}
+
+	for (int i = 0; i < NDATA; ++i)
+	{
+		if (data[i] == -1)
+			break;
+		printf("%d ", data[i]);
+	}
+
+	printf("\n");
+	//---------------------------------------
+	printf("\nfunction: 6\n");
+
+	int e = 0;
+	while (e < NDATA && data[e] != -1)
+		e++;
+
+	for (int i = 0; i < e - 1; ++i)
+	{
+		for (int j = 0; j < e - i - 1; ++j)
+		{
+			if (data[j] < data[j + 1])
+			{
+				int temp = data[j];
+				data[j] = data[j + 1];
+				data[j + 1] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < e; ++i)
+	{
+		printf("%d ", data[i]);
+	}
+
+	printf("\n");
+	//---------------------------------------
+	printf("\nfunction7 :\n");
+
+	data[e] = 78;
+	e++;
+
+	for (int i = 0; i < e - 1; ++i)
+	{
+		for (int j = 0; j < e - i - 1; ++j)
+		{
+			if (data[j] < data[j + 1])
+			{
+				int temp = data[j];
+				data[j] = data[j + 1];
+				data[j + 1] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < e; ++i)
+	{
+		printf("%d ", data[i]);
+	}
+
+	return 0;
+}
+
